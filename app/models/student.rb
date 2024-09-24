9cefdbd785e47d37ba3ab5736d062d4609a25a98
Fitsum Handiso <fhandis1@msudenver.edu>
2024-09-23 20:55:55 -0600

    I figured out how to add a profile pic, but I still have some questions.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,6 @@
 class Student < ApplicationRecord
     validates :school_email, uniqueness: true
     validates :first_name, :last_name, :school_email, :major, presence: true
-    #has_one_attached :profile_pic
+    has_one_attached :profile_pic
+    validates :profile_pic, presence: true
 end
