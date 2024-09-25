a2739329643a5ac910376d9793b74cb97642af12
Fitsum Handiso <fhandis1@msudenver.edu>
2024-09-24 18:26:46 -0600

    I know I should have written the tests before everything else, but they are written

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,5 +2,5 @@ class Student < ApplicationRecord
     validates :school_email, uniqueness: true
     validates :first_name, :last_name, :school_email, :major, presence: true
     has_one_attached :profile_pic
-    validates :profile_pic, presence: true
+    #validates :profile_pic, presence: true
 end
--- test/models/student_test.rb
+++ test/models/student_test.rb
@@ -1,7 +1,21 @@
 require "test_helper"
 
 class StudentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should raise error when saving student without first name" do
+    assert_raises ActiveRecord::RecordInvalid do
+      Student.create!(last_name: "Nikola", school_email: "jokic@msudenver.edu", major: "CS")
+    end
+  end
+  test "should raise error when saving student without last name" do
+    assert_raises ActiveRecord::RecordInvalid do
+      Student.create!(first_name: "James", school_email: "lebron@msudenver.edu", major: "Physics")
+    end
+  end
+
+  test "should raise an error when email is not unique" do
+    Student.create!(first_name: "Lebron", last_name: "James", school_email: "lebron@msudenver.edu", major: "Physics")
+    assert_raises ActiveRecord::RecordInvalid do
+      Student.create!(first_name: "Bronny", last_name: "James", school_email: "lebron@msudenver.edu", major: "Chem")
+    end
+  end
 end
